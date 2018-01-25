c358642a446672975414c747c05378cb76a2ef2c
Will <wjsrobertson@gmail.com>
2018-01-25 19:55:06 +0000

    merge

--- src/token-selector.js
+++ src/token-selector.js
@@ -1,4 +1,5 @@
 const $ = require('jquery')
+const _ = require('lodash')
 require('select2')(window, $)
 const configHelper = require('./config-helper.js')
 
@@ -8,8 +9,7 @@ function bind(selectElementId, onChange) {
     select = $(selectElementId)
     select.select2()
 
-    // TODO - sort me
-    const tokens = configHelper.tokens()
+    const tokens = _.sortBy(configHelper.tokens(), function(tk) { return tk.name })
     tokens.forEach((tk) => {
         select.append(new Option(tk.name, tk.addr, false, false))
     })
