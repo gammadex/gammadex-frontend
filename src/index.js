39b959fe8bf1ea8a731b3c459b6acf3e1e6d9b10
Will Robertson <wjsrobertson@gmail.com>
2018-01-28 20:10:16 +0000

    Cleanup - delete the unrequired lifecycle listener

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './pages/App.js'
-import LifecycleEventHandler from './LifecycleEventHandler'
 
-ReactDOM.render(<App />, document.getElementById('app'), () => {
-    LifecycleEventHandler.start()
-})
+ReactDOM.render(<App />, document.getElementById('app'))
