5d6c8022a382672a13ac64fac71ce993f0a98f94
Will Robertson <wjsrobertson@gmail.com>
2018-01-28 20:12:17 +0000

    Cleanup - delete unused ConfigActions

--- src/actions/ConfigActions.js
+++
@@ -1,9 +0,0 @@
-import dispatcher from "../dispatcher"
-import ActionNames from "./ActionNames"
-
-export function loadConfig() {
-    dispatcher.dispatch({
-        type: ActionNames.LOAD_CONFIG
-    })
-}
-
