cd85f1e1396e7846239f329600910c34fef0e691
Will Robertson <wjsrobertson@gmail.com>
2018-02-07 23:31:38 +0000

    Make buttons in trades history a bit smaller

--- src/components/OrderBook/TradeHistoryRow.js
+++ src/components/OrderBook/TradeHistoryRow.js
@@ -12,9 +12,9 @@ export default class OrdersTableRow extends React.Component {
                 <td>{trade.side}</td>
                 <td>{trade.date}</td>
                 <td>
-                    <a className="btn btn-primary table-button" target="_blank" rel="noopener" href={`https://etherscan.io/address/${trade.buyer}`}>Buyer</a>
-                    <a className="btn btn-primary table-button" target="_blank" rel="noopener" href={`https://etherscan.io/address/${trade.seller}`}>Seller</a>
-                    <a className="btn btn-primary table-button" target="_blank" rel="noopener" href={`https://etherscan.io/tx/${trade.txHash}`}>Transaction</a>
+                    <a className="btn btn-sm btn-primary table-button" target="_blank" rel="noopener" href={`https://etherscan.io/address/${trade.buyer}`}>Buyer</a>
+                    <a className="btn btn-sm btn-primary table-button" target="_blank" rel="noopener" href={`https://etherscan.io/address/${trade.seller}`}>Seller</a>
+                    <a className="btn btn-sm btn-primary table-button" target="_blank" rel="noopener" href={`https://etherscan.io/tx/${trade.txHash}`}>Transaction</a>
                 </td>
             </tr>
         )
