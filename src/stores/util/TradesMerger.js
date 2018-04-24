45b20a002c146d021e34ab3ed87f30a22967311e
Alex Mellusco <alexmellusco@gmail.com>
2018-04-24 17:10:17 +0100

    Trade de-dupe comment

--- src/stores/util/TradesMerger.js
+++ src/stores/util/TradesMerger.js
@@ -13,6 +13,12 @@ export function mergeAndSortTrades(existing, incoming, tokenAddress) {
     }
 }
 
+// TODO txHash is not guaranteed to be unique for a transaction, if a transaction emits multiple trades. E.g.
+// https://ropsten.etherscan.io/tx/0xedf7719ee66cceb355428f2c6e3881310cc0fd3d4b461f58e1647e9c0e73eed8
+// unique composite key should be txHash + logIndex
+//
+// (only gammadex socket api sends logIndex)
+// for the above tx, ED/FD would send repeating txHash - resulting in false de-duping.
 export function sortByTimeAndIdRemovingDuplicates(trades) {
     const deDuped = MessageUtils.removeDups(trades, 'txHash')
     const sorted = _.sortBy(_.sortBy(deDuped, t => t.id), t => t.date)
