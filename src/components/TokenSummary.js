d70db2015b2bbbc83f1a89063bffb8ac03a0f8a3
Will <wjsrobertson@gmail.com>
2018-05-25 09:24:24 +0100

    Display token stats even when the stats have not yet been retrieved

--- src/components/TokenSummary.js
+++ src/components/TokenSummary.js
@@ -29,53 +29,56 @@ export default class TokenSummary extends React.Component {
     }
 
     render() {
+        // TODO - handle the case when tokenAddress != token.address and clean up conditionals
         const {token} = this.props
         const {low, high, tokenVolume, ethVolume, last, percentChange, tokenAddress} = this.state.tradeStats
 
-        if (tokenAddress && token.address && tokenAddress.toLowerCase() && token.address.toLowerCase()) {
-
-            return (
-                <div className="card">
-                    <div className="card-header">
-                        <h1 className="main-header">{token.name} / ETH</h1>
-                    </div>
-
-                    <div className="card-body">
-                        <table className="table table-sm token-stats-table">
-                            <tbody>
-                            <tr>
-                                <td>Last Price</td>
-                                <td><Round price softZeros fallback="-">{last}</Round></td>
-                                <td>24h Change</td>
-                                <td><Round percent suffix="%" fallback="-"
-                                           classNameFunc={(num) => num > 0 ? 'buy-green' : 'sell-red'}>{percentChange}</Round>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>24h High</td>
-                                <td><Round price softZeros fallback="-">{high}</Round></td>
-                                <td>24h Low</td>
-                                <td><Round price softZeros fallback="-">{low}</Round></td>
-                            </tr>
-                            <tr>
-                                <td>24h Vol ETH</td>
-                                <td><Round fallback="-">{ethVolume}</Round></td>
-                                <td>24h Vol {token.name}</td>
-                                <td><Round fallback="-">{tokenVolume}</Round></td>
-                            </tr>
-                            </tbody>
-                        </table>
-                    </div>
-
-                    <div className="card-footer">
-                        <strong>{token.lName !== undefined ? token.lName : token.name}</strong> contract: <EtherScan
-                        type="address" address={token.address} display="truncate"/>
-                    </div>
-                </div>
-            )
-        } else {
-            return null
+        const title = token ? `${token.name} / ETH` : ''
+        const contract = token ? <EtherScan type="address" address={token.address} display="truncate"/> : ''
+        let name = ''
+        let longName = ''
+        if (token && tokenAddress === token.address) {
+            longName = token.lName ? token.lName : token.name
         }
+
+        return (
+            <div className="card">
+                <div className="card-header">
+                    <h1 className="main-header">{title}</h1>
+                </div>
+
+                <div className="card-body">
+                    <table className="table table-sm token-stats-table">
+                        <tbody>
+                        <tr>
+                            <td>Last Price</td>
+                            <td><Round price softZeros fallback="-">{last}</Round></td>
+                            <td>24h Change</td>
+                            <td><Round percent suffix="%" fallback="-"
+                                       classNameFunc={(num) => num > 0 ? 'buy-green' : 'sell-red'}>{percentChange}</Round>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>24h High</td>
+                            <td><Round price softZeros fallback="-">{high}</Round></td>
+                            <td>24h Low</td>
+                            <td><Round price softZeros fallback="-">{low}</Round></td>
+                        </tr>
+                        <tr>
+                            <td>24h Vol ETH</td>
+                            <td><Round fallback="-">{ethVolume}</Round></td>
+                            <td>24h Vol {name}</td>
+                            <td><Round fallback="-">{tokenVolume}</Round></td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
+
+                <div className="card-footer">
+                    <strong>{longName}</strong> contract: {contract}
+                </div>
+            </div>
+        )
     }
 }
 
