80647319b79ee9fb42579f2f638473b35260dcfe
Alex Mellusco <alexmellusco@gmail.com>
2018-05-27 09:10:46 +0100

    Add Network Info to Wallet Popover

--- src/components/Account.js
+++ src/components/Account.js
@@ -4,34 +4,40 @@ import * as WalletActions from "../actions/WalletActions"
 import * as AccountActions from "../actions/AccountActions"
 import * as WalletDao from "../util/WalletDao"
 import AccountStore from "../stores/AccountStore"
+import WalletStore from "../stores/WalletStore"
 import Conditional from "./CustomComponents/Conditional"
 import * as LifeCycleActions from "../actions/LifecycleActions"
 import EtherDeltaWeb3 from "../EtherDeltaWeb3"
 import { toDataUrl } from '../lib/blockies.js'
 import { truncate } from "../util/FormatUtil"
-import { Box, BoxFooter, BoxSection } from "./CustomComponents/Box"
+import { Box, BoxFooter, BoxSection, BoxHeader } from "./CustomComponents/Box"
 import TruncatedAddress from "../components/CustomComponents/TruncatedAddress"
 import Config from '../Config'
 import AccountType from '../AccountType'
+import * as EthereumNetworks from "../util/EthereumNetworks"
 
 export default class Account extends React.Component {
     constructor(props) {
         super(props)
         this.onAccountStoreChange = this.onAccountStoreChange.bind(this)
+        this.onWalletStoreChange = this.onWalletStoreChange.bind(this)
         this.toggerPopover = this.toggerPopover.bind(this)
     }
     state = {
         account: null,
         accountPopoverOpen: false,
-        selectedAccountType: null
+        selectedAccountType: null,
+        providedWeb3: {}
     }
 
     componentWillMount() {
         AccountStore.on("change", this.onAccountStoreChange)
+        WalletStore.on("change", this.onWalletStoreChange)
     }
 
     componentWillUnmount() {
         AccountStore.removeListener("change", this.onAccountStoreChange)
+        WalletStore.removeListener("change", this.onWalletStoreChange)
     }
 
     onAccountStoreChange() {
@@ -40,6 +46,12 @@ export default class Account extends React.Component {
             account: accountState.account,
             accountPopoverOpen: accountState.accountPopoverOpen,
             selectedAccountType: accountState.selectedAccountType
+        })
+    }
+
+    onWalletStoreChange() {
+        this.setState({
+            providedWeb3: WalletStore.getProvidedWeb3Info()
         })
     }
 
@@ -54,31 +66,53 @@ export default class Account extends React.Component {
     }
 
     render() {
-        const { account, accountPopoverOpen, selectedAccountType } = this.state
+        const { account, accountPopoverOpen, selectedAccountType, providedWeb3 } = this.state
+        const { available, isMainNet, netDescription, accountAvailable } = providedWeb3
         const truncatedAccount = account ? truncate(account, { left: 7, right: 5 }) : ""
         const accountOrEmpty = account ? account : ""
         //const accountLink = <TruncatedAddress url={`${Config.getEtherscanUrl()}/address/${accountOrEmpty}`}>{accountOrEmpty}</TruncatedAddress>
-        const accountLink = <a target="_blank" rel="noopener noreferrer" href={`${Config.getEtherscanUrl()}/address/${accountOrEmpty}`}>{accountOrEmpty}</a>
+        const accountLink = <small><a target="_blank" rel="noopener noreferrer" href={`${Config.getEtherscanUrl()}/address/${accountOrEmpty}`}>{accountOrEmpty}</a></small>
         const accountTypeName = (selectedAccountType === AccountType.METAMASK ? "MetaMask" : "Wallet")
         return (
             <div id="accountTop" className="form-group ml-1">
-                <Conditional displayCondition={account}>
+                <Conditional displayCondition={account != null}>
                     <img src={account == null ? null : toDataUrl(account)} className="wallet" />
                 </Conditional>
-                <Conditional displayCondition={account}>
+                <Conditional displayCondition={account != null}>
                     <input type="button" className="btn" onClick={this.toggerPopover} value={truncatedAccount} />
                     <Popover placement="bottom" isOpen={accountPopoverOpen} target="accountTop" toggle={this.toggerPopover}>
                         <div className="shadow gas-prices">
                             <PopoverBody>
-                                <Box title="Wallet Details">
+                                <Box>
+                                    <BoxHeader>
+                                        <div className="row">
+                                            <div className="col-lg-12 text-center">
+                                                <Conditional displayCondition={selectedAccountType === AccountType.METAMASK}>
+                                                    <img src="https://metamask.io/img/favicon-16x16.png" />
+                                                    <br />
+                                                    <br />
+                                                </Conditional>
+                                                <strong>{accountTypeName} Connected</strong>
+                                            </div>
+                                        </div>
+                                    </BoxHeader>
                                     <BoxSection>
                                         <div className="row">
                                             <div className="col-lg-12 text-center">
-                                                Etherscan:
+                                                <Conditional displayCondition={selectedAccountType != AccountType.METAMASK}>
+                                                    <span className="fas fa-check-circle buy-green" />&nbsp;{EthereumNetworks.getMainNetDescription()}
+                                                </Conditional>
+                                                <Conditional displayCondition={selectedAccountType === AccountType.METAMASK && isMainNet === true}>
+                                                    <span className="fas fa-check-circle buy-green" />&nbsp;{netDescription}
+                                                </Conditional>
+                                                <Conditional displayCondition={selectedAccountType === AccountType.METAMASK && isMainNet === false}>
+                                                    <span className="fas fa-exclamation-triangle sell-red" />&nbsp;{netDescription}
+                                                </Conditional>
+                                                <br />
+                                                <br />
+                                                Address (open in Etherscan)
                                                 <br />
                                                 {accountLink}
-                                                <br />
-                                                <Badge id="atName" color="secondary">{accountTypeName}</Badge>
                                             </div>
                                         </div>
                                     </BoxSection>
