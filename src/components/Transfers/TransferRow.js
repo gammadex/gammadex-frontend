d7d31de665cd29169c67b5e4466c1ce5ec832839
Alex Mellusco <alexmellusco@gmail.com>
2018-06-05 09:04:38 +0100

    String prop should be boolean warning

--- src/components/Transfers/TransferRow.js
+++ src/components/Transfers/TransferRow.js
@@ -14,7 +14,7 @@ export default class TransferRow extends React.Component {
             <tr  className={refreshClass}>
                 <td>{tokenName}</td>
                 <td><Round price softZeros>{amount}</Round></td>
-                <td><Date year="true">{date}</Date></td>
+                <td><Date year={true}>{date}</Date></td>
                 <td><span>{status}</span></td>
                 <td><Etherscan type="tx" address={txHash} display="icon"/></td>
             </tr>
