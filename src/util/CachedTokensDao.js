6f61ef10ff2adbbdb9ddb44cef847fcebf8b63ed
Will Robertson <wjsrobertson@gmail.com>
2018-06-08 12:58:07 +0100

    Different token config for different envs

--- src/util/CachedTokensDao.js
+++ src/util/CachedTokensDao.js
@@ -1,8 +1,14 @@
 import _ from "lodash"
+import Config from '../Config'
+
+function key(name) {
+    return Config.getReactEnv() + '.' + name
+}
 
 export function getCachedServerTokensAndVersion() {
-    if (localStorage.cachedTokens) {
-        const cachedTokens = JSON.parse(localStorage.cachedTokens)
+
+    if (localStorage[key('cachedTokens')]) {
+        const cachedTokens = JSON.parse(localStorage[key('cachedTokens')])
         const version = parseInt(cachedTokens.version)
         const tokens = cachedTokens.list
 
@@ -15,17 +21,17 @@ export function getCachedServerTokensAndVersion() {
 }
 
 export function saveServerTokensAndVersion(tokensAndVersion) {
-    localStorage.cachedTokens = JSON.stringify(tokensAndVersion)
+    localStorage[key('cachedTokens')] = JSON.stringify(tokensAndVersion)
 }
 
 export function getUserTokens() {
-    if (localStorage.userTokenList) {
-        return JSON.parse(localStorage.userTokenList)
+    if (localStorage[key('userTokenList')]) {
+        return JSON.parse(localStorage[key('userTokenList')])
     }
 
     return []
 }
 
 export function saveUserTokens(userTokens) {
-    localStorage.userTokenList = JSON.stringify(userTokens)
+    localStorage[key('userTokenList')] = JSON.stringify(userTokens)
 }
