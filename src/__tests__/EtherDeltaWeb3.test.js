23e8ee9386f43afe891413c93a9215fa554a95e5
Alex Mellusco <alexmellusco@gmail.com>
2018-06-25 14:23:06 +0100

    Test coverage of critical codepaths #117

--- src/__tests__/EtherDeltaWeb3.test.js
+++ src/__tests__/EtherDeltaWeb3.test.js
@@ -3,145 +3,262 @@ import EtherDeltaWeb3 from "../EtherDeltaWeb3"
 import Config from '../Config'
 import abiEtherDelta from '../config/etherdelta.json'
 import etherDeltaBytecode from '../config/etherdeltabytecode.json'
+import abiTestToken from '../config/testtoken.json'
+import testTokenBytecode from '../config/testtokenbytecode.json'
 import BigNumber from 'bignumber.js'
+import _ from "lodash"
 
 const web3 = new Web3(new Web3.providers.HttpProvider(Config.getWeb3Url()))
 
-const deploymentAddress = '0xfcad0b19bb29d4674531d6f115237e16afce377c'
-const deploymentPrivateKey = '0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef'
+// This is the primary (only) account on the ganache node, which is used to deploy contracts and as a metamask proxy
+const metamaskAddress = '0xfcad0b19bb29d4674531d6f115237e16afce377c'
 
-const pkAccount = web3.eth.accounts.create()
+// This is a dynamic account local to this test and the running instance of the smart contract
+const primaryKeyAccount = web3.eth.accounts.create()
 
-const weiGasPrice = '3000000000'
+const threeGwei = web3.utils.toWei('3', 'gwei')
 
 beforeAll(() => {
+    /**
+     * Deploy EtherDelta smart contract to ganache, capturing the contract instance globally
+     * and shoe-horn that (dynamic) address into our test configuration
+     */
+
     const edContract = new web3.eth.Contract(abiEtherDelta)
-    return edContract.deploy({
-        data: `0x${etherDeltaBytecode.data}`,
-        arguments: [deploymentAddress, deploymentAddress, "0x0", 0, 3000000000000000, 0]
+    const promiseDeployEdContract = edContract.deploy({
+        data: etherDeltaBytecode.data,
+        arguments: [metamaskAddress, metamaskAddress, "0x0", 0, 3000000000000000, 0]
     }).send({
-        from: deploymentAddress,
+        from: metamaskAddress,
         gas: 3000000,
-        gasPrice: weiGasPrice
+        gasPrice: threeGwei
     }).then(newContractInstance => {
         global.edContractInstance = newContractInstance
-
         Config.getEnv().etherDeltaAddress = newContractInstance.options.address
-
-        return web3.eth.sendTransaction({
-            from: deploymentAddress,
-            to: pkAccount.address,
-            value: web3.utils.toWei('10', 'ether')
-        })
     })
+
+    /**
+     * Deploy ERC-20 Test Token to ganache. The contract by default gives the owner (metamask) account 100 TEST
+     */
+    const testTokenContract = new web3.eth.Contract(abiTestToken)
+    const promiseDeployTestTokenContract = testTokenContract.deploy({
+        data: testTokenBytecode.data
+    }).send({
+        from: metamaskAddress,
+        gas: 4700000,
+        gasPrice: threeGwei
+    }).then(newContractInstance => {
+        global.testTokenContractInstance = newContractInstance
+
+        // Transfer 10 TEST from the metamask to the private key account
+        return newContractInstance.methods.transfer(primaryKeyAccount.address, web3.utils.toWei('10', 'ether'))
+            .send({
+                from: metamaskAddress,
+                gas: 200000,
+                gasPrice: threeGwei
+            })
+    })
+
+    // Transfer 10 ETH from the metamask to the private key account
+    const promiseTopupWalletEth = web3.eth.sendTransaction({
+        from: metamaskAddress,
+        to: primaryKeyAccount.address,
+        value: web3.utils.toWei('10', 'ether')
+    })
+
+
+    return promiseDeployEdContract
+        .then(promiseTopupWalletEth)
+        .then(promiseDeployTestTokenContract)
+
 })
 
+function contractBalanceTest(promiseAction = () => { }, userAddress, tokenAddress, weiExpectedAmountDiff) {
+    return edContractInstance.methods.balanceOf(tokenAddress, userAddress).call()
+        .then(balBefore => {
+            return promiseAction()
+                .then(() => {
+                    return edContractInstance.methods.balanceOf(tokenAddress, userAddress).call()
+                        .then(balAfter => expect(balAfter).toEqual(
+                            BigNumber(balBefore).plus(BigNumber(weiExpectedAmountDiff)).toFixed()))
+                })
+        })
+}
+
+function ethWalletBalanceTest(promiseAction = () => { }, userAddress, weiGasPrice, weiExpectedAmountDiff) {
+    return web3.eth.getBalance(userAddress).then(balBefore => {
+        return promiseAction()
+            .then((receipt) => {
+                // Tx Cost = Gas Price * Gas Used
+                const weiGasCost = BigNumber(receipt.gasUsed).times(weiGasPrice)
+                return web3.eth.getBalance(userAddress).then(balAfter => {
+                    expect(balAfter).toEqual(BigNumber(balBefore).plus(weiExpectedAmountDiff.minus(weiGasCost)).toFixed())
+                })
+            })
+    })
+}
+
+function gasTest(promiseAction = () => { }, weiGasPrice, gasLimit) {
+    return promiseAction()
+        .then(receipt => {
+            return web3.eth.getTransaction(receipt.transactionHash)
+                .then(tx => {
+                    expect(tx.gasPrice).toEqual(weiGasPrice)
+                    expect(tx.gas).toEqual(gasLimit)
+                })
+        })
+}
+
+function eventFromReceipt(receipt, eventName) {
+    if (receipt.events) {
+        // meta mask
+        return receipt.events[eventName].returnValues
+    } else {
+        // private key
+        const eventJsonInterface = _.find(
+            edContractInstance.options.jsonInterface,
+            o => o.name === eventName && o.type === 'event',
+        )
+        return web3.eth.abi.decodeLog(eventJsonInterface.inputs, receipt.logs[0].data, receipt.logs[0])
+    }
+}
+
+function testTokenAddress() {
+    return testTokenContractInstance.options.address
+}
 function testContract(userAddress) {
     describe('promiseDepositEther', () => {
         test('should increase exchange ETH balance', () => {
-            const weiTwoEther = web3.utils.toWei('2', 'ether')
-            return EtherDeltaWeb3.promiseDepositEther(userAddress, nonce, weiTwoEther, weiGasPrice)
-                .then(() => {
-                    return edContractInstance.methods.balanceOf(Config.getBaseAddress(), userAddress).call()
-                        .then(balance => expect(balance).toEqual(weiTwoEther))
-                })
+            const weiTestAmount = web3.utils.toWei('0.11', 'ether')
+            return contractBalanceTest(
+                () => EtherDeltaWeb3.promiseDepositEther(userAddress, nonce, weiTestAmount, web3.utils.toWei('1.11', 'gwei')),
+                userAddress, Config.getBaseAddress(), weiTestAmount
+            )
         })
         test('should decrease wallet ETH balance (accounting for gas fee)', () => {
-            const weiTwoEther = web3.utils.toWei('2', 'ether')
-            return web3.eth.getBalance(userAddress).then(balanceBeforeDeposit => {
-                return EtherDeltaWeb3.promiseDepositEther(userAddress, nonce, weiTwoEther, weiGasPrice)
-                    .then((receipt) => {
-                        return web3.eth.getBalance(userAddress).then(balanceAfterDeposit => {
-                            // End ETH Balance = Start ETH Balance - Deposit Amount - Tx Cost
-                            // Tx Cost = Gas Price * Gas Used
-                            expect(balanceAfterDeposit).toEqual(BigNumber(balanceBeforeDeposit)
-                                .minus(BigNumber(weiTwoEther)).minus(BigNumber(receipt.gasUsed).times(weiGasPrice)).toFixed())
-                        })
-                    })
-            })
+            const weiTestAmount = web3.utils.toWei('0.12', 'ether')
+            const weiGasPrice = web3.utils.toWei('1.12', 'gwei')
+            return ethWalletBalanceTest(
+                () => EtherDeltaWeb3.promiseDepositEther(userAddress, nonce, weiTestAmount, weiGasPrice),
+                userAddress, weiGasPrice, BigNumber(weiTestAmount).negated()
+            )
         })
 
         test('should use the gas settings provided by the user and app', () => {
-            const weiTwoEther = web3.utils.toWei('2', 'ether')
-            return EtherDeltaWeb3.promiseDepositEther(userAddress, nonce, weiTwoEther, weiGasPrice)
+            const weiGasPrice = web3.utils.toWei('1.13', 'gwei')
+            return gasTest(
+                () => EtherDeltaWeb3.promiseDepositEther(userAddress, nonce, web3.utils.toWei('0.13', 'ether'), weiGasPrice),
+                weiGasPrice,
+                Config.getGasLimit('deposit')
+            )
+        })
+
+        test('ETH value sent with transaction should equal the deposit amount', () => {
+            const weiDepositAmount = web3.utils.toWei('0.14', 'ether')
+            return EtherDeltaWeb3.promiseDepositEther(userAddress, nonce, weiDepositAmount, web3.utils.toWei('1.14', 'gwei'))
                 .then(receipt => {
                     return web3.eth.getTransaction(receipt.transactionHash)
                         .then(tx => {
-                            expect(tx.gasPrice).toEqual(weiGasPrice)
-                            expect(tx.value).toEqual(weiTwoEther)
-                            expect(tx.gas).toEqual(Config.getGasLimit('deposit'))
+                            expect(tx.value).toEqual(weiDepositAmount)
                         })
                 })
         })
 
-        test('ETH value sent with transaction should equal the deposit amount', () => {
-            const weiTwoEther = web3.utils.toWei('2', 'ether')
-            return EtherDeltaWeb3.promiseDepositEther(userAddress, nonce, weiTwoEther, weiGasPrice)
+        test('Should generate a Deposit Event for this user and ETH deposit amount', () => {
+            const weiDepositAmount = web3.utils.toWei('0.15', 'ether')
+            return EtherDeltaWeb3.promiseDepositEther(userAddress, nonce, weiDepositAmount, web3.utils.toWei('1.15', 'gwei'))
                 .then(receipt => {
-                    return web3.eth.getTransaction(receipt.transactionHash)
-                        .then(tx => {
-                            expect(tx.value).toEqual(weiTwoEther)
-                        })
+                    const { token, user, amount } = eventFromReceipt(receipt, 'Deposit')
+                    expect(token).toEqual(Config.getBaseAddress())
+                    expect(user.toLowerCase()).toEqual(userAddress.toLowerCase())
+                    expect(amount).toEqual(weiDepositAmount)
                 })
         })
     })
 
     describe('promiseWithdrawEther', () => {
         test('should decrease exchange ETH balance', () => {
-            const weiOneEther = web3.utils.toWei('1', 'ether')
-            return EtherDeltaWeb3.promiseWithdrawEther(userAddress, nonce, weiOneEther, weiGasPrice)
-                .then(() => {
-                    return edContractInstance.methods.balanceOf(Config.getBaseAddress(), userAddress).call()
-                        .then(balance => expect(balance).toEqual(web3.utils.toWei('7', 'ether')))
-                })
+            const weiTestAmount = web3.utils.toWei('0.21', 'ether')
+            return contractBalanceTest(
+                () => EtherDeltaWeb3.promiseWithdrawEther(userAddress, nonce, weiTestAmount, web3.utils.toWei('1.21', 'gwei')),
+                userAddress, Config.getBaseAddress(), BigNumber(weiTestAmount).negated())
         })
         test('should increase wallet ETH balance (accounting for gas fee)', () => {
-            const weiOneEther = web3.utils.toWei('1', 'ether')
-            return web3.eth.getBalance(userAddress).then(balanceBeforeDeposit => {
-                return EtherDeltaWeb3.promiseWithdrawEther(userAddress, nonce, weiOneEther, weiGasPrice)
-                    .then((receipt) => {
-                        return web3.eth.getBalance(userAddress).then(balanceAfterDeposit => {
-                            // End ETH Balance = Start ETH Balance - Deposit Amount - Tx Cost
-                            // Tx Cost = Gas Price * Gas Used
-                            expect(balanceAfterDeposit).toEqual(BigNumber(balanceBeforeDeposit)
-                                .plus(BigNumber(weiOneEther)).minus(BigNumber(receipt.gasUsed).times(weiGasPrice)).toFixed())
-                        })
-                    })
-            })
+            const weiTestAmount = web3.utils.toWei('0.22', 'ether')
+            const weiGasPrice = web3.utils.toWei('1.22', 'gwei')
+            return ethWalletBalanceTest(
+                () => EtherDeltaWeb3.promiseDepositEther(userAddress, nonce, weiTestAmount, weiGasPrice),
+                userAddress, weiGasPrice, BigNumber(weiTestAmount).negated()
+            )
         })
 
         test('should use the gas settings provided by the user and app', () => {
-            const weiOneEther = web3.utils.toWei('1', 'ether')
-            return EtherDeltaWeb3.promiseWithdrawEther(userAddress, nonce, weiOneEther, weiGasPrice)
+            const weiGasPrice = web3.utils.toWei('1.23', 'gwei')
+            return gasTest(
+                () => EtherDeltaWeb3.promiseWithdrawEther(userAddress, nonce, web3.utils.toWei('0.23', 'ether'), weiGasPrice),
+                weiGasPrice,
+                Config.getGasLimit('withdraw')
+            )
+        })
+
+        test('Should generate a Withdraw Event for this user and ETH withdraw amount', () => {
+            const weiWithdrawAmount = web3.utils.toWei('0.24', 'ether')
+            return EtherDeltaWeb3.promiseWithdrawEther(userAddress, nonce, weiWithdrawAmount, web3.utils.toWei('1.24', 'gwei'))
                 .then(receipt => {
-                    return web3.eth.getTransaction(receipt.transactionHash)
-                        .then(tx => {
-                            expect(tx.gasPrice).toEqual(weiGasPrice)
-                            expect(tx.gas).toEqual(Config.getGasLimit('withdraw'))
-                        })
+                    const { token, user, amount } = eventFromReceipt(receipt, 'Withdraw')
+                    expect(token).toEqual(Config.getBaseAddress())
+                    expect(user.toLowerCase()).toEqual(userAddress.toLowerCase())
+                    expect(amount).toEqual(weiWithdrawAmount)
                 })
         })
+    })
+
+    describe('promiseTokenApprove', () => {
+
+        beforeEach(() => {
+            // Per the test contract:
+            // once you have approved an amount, the only way to reduce that approval amount is to:
+            // - transfer it
+            // - reset the approval amount to zero (which we do hear so future tests have clean state)
+            return EtherDeltaWeb3.promiseTokenApprove(userAddress, nonce, testTokenAddress(), '0', threeGwei)
+                .then(() => web3.eth.getTransactionCount(primaryKeyAccount.address).then(count => global.nonce = count))
+        })
+
+        test(`should allow the ED contract to deposit an amount of token on the user's behalf`, () => {
+            const weiTestAmount = web3.utils.toWei('0.31', 'ether')
+            return EtherDeltaWeb3.promiseTokenApprove(userAddress, nonce, testTokenAddress(), weiTestAmount, web3.utils.toWei('1.31', 'gwei'))
+                .then(() => {
+                    return testTokenContractInstance.methods.allowance(userAddress, Config.getEtherDeltaAddress()).call()
+                        .then(allowance => expect(allowance).toEqual(weiTestAmount))
+                })
+        })
+
+        test('should use the gas settings provided by the user and app', () => {
+            const weiGasPrice = web3.utils.toWei('1.32', 'gwei')
+            return gasTest(
+                () => EtherDeltaWeb3.promiseTokenApprove(userAddress, nonce, testTokenAddress(), web3.utils.toWei('0.32', 'ether'), weiGasPrice),
+                weiGasPrice,
+                Config.getGasLimit('approveTokenDeposit')
+            )
+        })      
     })
 }
 
 describe('WalletAccountProvider', () => {
 
     beforeAll(() => {
-        EtherDeltaWeb3.initForPrivateKey(pkAccount.address, pkAccount.privateKey.slice(2))
+        EtherDeltaWeb3.initForPrivateKey(primaryKeyAccount.address, primaryKeyAccount.privateKey.slice(2))
     })
 
     beforeEach(() => {
-        return web3.eth.getTransactionCount(pkAccount.address)
-            .then(count => {
-                global.nonce = count
-            })
+        return web3.eth.getTransactionCount(primaryKeyAccount.address).then(count => global.nonce = count)
     })
 
     test('refreshAccount should return wallet address and nonce', () => {
         return EtherDeltaWeb3.refreshAccount()
             .then(res => {
                 const { address, nonce } = res
-                expect(address.toLowerCase()).toEqual(pkAccount.address.toLowerCase())
+                expect(address.toLowerCase()).toEqual(primaryKeyAccount.address.toLowerCase())
                 expect(nonce).toEqual(nonce)
             })
     })
@@ -151,7 +268,7 @@ describe('WalletAccountProvider', () => {
             .then(nonce => expect(nonce).toEqual(nonce))
     })
 
-    testContract(pkAccount.address)
+    testContract(primaryKeyAccount.address)
 })
 
 describe('MetaMaskAccountProvider', () => {
@@ -166,7 +283,7 @@ describe('MetaMaskAccountProvider', () => {
         return EtherDeltaWeb3.refreshAccount()
             .then(res => {
                 const { address, nonce } = res
-                expect(address.toLowerCase()).toEqual(deploymentAddress.toLowerCase())
+                expect(address.toLowerCase()).toEqual(metamaskAddress.toLowerCase())
                 expect(nonce).toEqual(0)
             })
     })
@@ -176,6 +293,6 @@ describe('MetaMaskAccountProvider', () => {
             .then(nonce => expect(nonce).toEqual(0))
     })
 
-    testContract(deploymentAddress)
+    testContract(metamaskAddress)
 })
 
