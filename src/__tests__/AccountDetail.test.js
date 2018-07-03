668fa0d8c3988f07bcd76815b3087f7d3bb6b837
Alex Mellusco <alexmellusco@gmail.com>
2018-07-03 18:31:45 +0100

    Test deposit and withdrawal of 9-decimal token, #117

--- src/__tests__/AccountDetail.test.js
+++ src/__tests__/AccountDetail.test.js
@@ -13,6 +13,8 @@ import AccountType from "../AccountType"
 import * as AccountApi from "../apis/AccountApi"
 import FundingStore from "../stores/FundingStore"
 import AccountStore from "../stores/AccountStore"
+import TokenStore from "../stores/TokenStore"
+
 import TransferStore from "../stores/TransferStore"
 import FundingState from "../components/Account/FundingState"
 
@@ -371,4 +373,153 @@ describe('AccountDetail', () => {
             promiseWithdrawTokenMock.mockRestore()
         })
     })    
+
+    describe('User deposits 3 DEF test tokens (9 decimals)', () => {
+        beforeAll(() => {
+            TokenStore.listedTokens[0].decimals = 9
+            TokenActions.selectToken({
+                address: testTokenContractInstance.options.address,
+                decimals: 9,
+                isListed: true,
+                name: null,
+                symbol: 'DEF'
+            })
+        })
+        it('should propogate value through to store and back to UI, when user updates the tok deposit amount', () => {
+            const value = '3'
+            wrapper.find('#tokDepositAmount').hostNodes().simulate('change', { target: { value } })
+            const { tokDepositAmountControlled, tokDepositAmountWei } = FundingStore.getFundingState()
+            expect(tokDepositAmountControlled).toEqual(value)
+            expect(tokDepositAmountWei.toString()).toEqual(BigNumber(value).times(BigNumber(10 ** 9)).toString())
+            expect(wrapper.find('#tokDepositAmount').hostNodes().props().value).toEqual(value)
+            const fundingWrapper = wrapper.find(Funding)
+            expect(FundingStore.getFundingState().tokDepositState).toEqual(FundingState.OK)
+            expect(fundingWrapper.instance().tokDepositInputProps().tokDepositValid).toEqual(true)
+        })
+
+        it('should display a confirmation modal when the user submits a token deposit', () => {
+            const value = '3'
+            wrapper.find('#tokDepositAmount').hostNodes().simulate('change', { target: { value } })
+            wrapper.find('#tokDepositAmountButton').hostNodes().simulate('click')
+            const { modalType, modalText } = FundingStore.getFundingState()
+            expect(modalType).toEqual(FundingModalType.TOK_DEPOSIT)
+            expect(modalText).toEqual(`Deposit ${value} DEF to exchange?`)
+            expect(wrapper.find(Modal).instance().props.isOpen).toEqual(true)
+            expect(wrapper.find('#fundingModalBody').hostNodes().text()).toEqual(`Deposit ${value} DEF to exchange?`)
+        })
+
+        it('should execute a token approve when the user confirms the modal', () => {
+            const promiseTokenApproveMock = jest.spyOn(EtherDeltaWeb3, "promiseTokenApprove")
+            promiseTokenApproveMock.mockImplementation(() => {
+                const promiEvent = Web3PromiEvent()
+                promiEvent.eventEmitter.emit('transactionHash', '1234')
+                return promiEvent.eventEmitter
+            })
+
+            const value = '3'
+            wrapper.find('#tokDepositAmount').hostNodes().simulate('change', { target: { value } })
+            wrapper.find('#tokDepositAmountButton').hostNodes().simulate('click')
+            wrapper.find('#fundingModalConfirmButton').hostNodes().simulate('click')
+
+            expect(promiseTokenApproveMock).toHaveBeenCalledWith(
+                primaryKeyAccount.address,
+                5,
+                testTokenContractInstance.options.address,
+                BigNumber(value).times(BigNumber(10 ** 9)),
+                BigNumber(Web3.utils.toWei('6', 'gwei')))
+
+            promiseTokenApproveMock.mockRestore()
+        })
+        it('should execute a token deposit when the user confirms the modal', (done) => {
+
+            const promiseDepositTokenMock = jest.spyOn(EtherDeltaWeb3, "promiseDepositToken")
+            promiseDepositTokenMock.mockImplementation(() => {
+                const promiEvent = Web3PromiEvent()
+                promiEvent.eventEmitter.emit('transactionHash', '123')
+                return promiEvent.eventEmitter
+            })
+
+            const value = '3'
+            wrapper.find('#tokDepositAmount').hostNodes().simulate('change', { target: { value } })
+            wrapper.find('#tokDepositAmountButton').hostNodes().simulate('click')
+            wrapper.find('#fundingModalConfirmButton').hostNodes().simulate('click')
+
+            // there is an async period between the token approval and the deposit
+            setTimeout(() => {
+                expect(promiseDepositTokenMock).toHaveBeenCalledWith(
+                    primaryKeyAccount.address,
+                    6,
+                    testTokenContractInstance.options.address,
+                    BigNumber(value).times(BigNumber(10 ** 9)),
+                    BigNumber(Web3.utils.toWei('6', 'gwei')))
+
+                promiseDepositTokenMock.mockRestore()
+
+                // need to reset the approval amount to zero in the token smart contract (so we can deposit tokens in the next test)
+                return EtherDeltaWeb3.promiseTokenApprove(primaryKeyAccount.address, 6, testTokenContractInstance.options.address, BigNumber(0), BigNumber(Web3.utils.toWei('6', 'gwei')))
+                    .then(res => {
+                        done()
+                    })
+            }, 2000)
+
+        })
+    })
+
+    describe('User withdraws 2 ABC test tokens (9 decimals)', () => {
+        it('should propogate value through to store and back to UI, when user updates the eth withdraw amount', (done) => {
+            const value = '2'
+            wrapper.find('#tokDepositAmount').hostNodes().simulate('change', { target: { value } })
+            wrapper.find('#tokDepositAmountButton').hostNodes().simulate('click')
+            wrapper.find('#fundingModalConfirmButton').hostNodes().simulate('click')
+            setTimeout(() => {
+                const value = '2'
+                wrapper.find('#tokWithdrawAmount').hostNodes().simulate('change', { target: { value } })
+    
+                const { tokWithdrawalAmountControlled, tokWithdrawalAmountWei } = FundingStore.getFundingState()
+                expect(tokWithdrawalAmountControlled).toEqual(value)
+                expect(tokWithdrawalAmountWei.toString()).toEqual(BigNumber(value).times(BigNumber(10 ** 9)).toString())
+    
+                expect(wrapper.find('#tokWithdrawAmount').hostNodes().props().value).toEqual(value)
+    
+                const fundingWrapper = wrapper.find(Funding)
+                expect(FundingStore.getFundingState().tokWithdrawalState).toEqual(FundingState.OK)
+                expect(fundingWrapper.instance().tokWithdrawalInputProps().tokWithdrawalValid).toEqual(true)
+                done()
+            }, 2000)
+        })
+
+        it('should display a confirmation modal when the user submits a token withdrawal', () => {
+            const value = '2'
+            wrapper.find('#tokWithdrawAmount').hostNodes().simulate('change', { target: { value } })
+            wrapper.find('#tokWithdrawAmountButton').hostNodes().simulate('click')
+            const { modalType, modalText } = FundingStore.getFundingState()
+            expect(modalType).toEqual(FundingModalType.TOK_WITHDRAWAL)
+            expect(modalText).toEqual(`Withdraw ${value} DEF from exchange?`)
+            expect(wrapper.find(Modal).instance().props.isOpen).toEqual(true)
+            expect(wrapper.find('#fundingModalBody').hostNodes().text()).toEqual(`Withdraw ${value} DEF from exchange?`)
+        })
+
+        it('should execute the token withdrawal when the user confirms the modal', () => {
+            const promiseWithdrawTokenMock = jest.spyOn(EtherDeltaWeb3, "promiseWithdrawToken")
+            promiseWithdrawTokenMock.mockImplementation(() => {
+                const promiEvent = Web3PromiEvent()
+                promiEvent.eventEmitter.emit('transactionHash', '123')
+                return promiEvent.eventEmitter
+            })
+            const value = '2'
+            wrapper.find('#tokWithdrawAmount').hostNodes().simulate('change', { target: { value } })
+            wrapper.find('#tokWithdrawAmountButton').hostNodes().simulate('click')
+            wrapper.find('#fundingModalConfirmButton').hostNodes().simulate('click')
+
+            expect(promiseWithdrawTokenMock).toHaveBeenCalledWith(
+                primaryKeyAccount.address,
+                9,
+                testTokenContractInstance.options.address,
+                BigNumber(value).times(BigNumber(10 ** 9)),
+                BigNumber(Web3.utils.toWei('6', 'gwei')))
+
+            promiseWithdrawTokenMock.mockRestore()
+        })
+    })    
+
 })
