6dca09c7192bcd2df4fb75e1f245d667fd72496a
Alex Mellusco <alexmellusco@gmail.com>
2018-07-09 11:17:39 +0100

    Add Line chart with colored volume bars to Chart POC

--- src/components/OrderBook/ChartRenderer.js
+++ src/components/OrderBook/ChartRenderer.js
@@ -11,11 +11,15 @@ import { min, max } from "d3-array"
 import { scaleTime } from "d3-scale"
 import { utcDay, utcHour, timeHour, utcMinute } from "d3-time"
 
+import ReactChartType from "./ReactChartType"
 
 import { ChartCanvas, Chart, ZoomButtons } from "react-stockcharts"
 import {
     BarSeries,
-    CandlestickSeries
+    CandlestickSeries,
+    LineSeries,
+    CircleMarker,
+    ScatterSeries
 } from "react-stockcharts/lib/series"
 import { XAxis, YAxis } from "react-stockcharts/lib/axes"
 import {
@@ -68,7 +72,7 @@ function tooltipContent() {
     }
 }
 
-class CandleStickChartWithDarkTheme extends React.Component {
+class ChartRenderer extends React.Component {
 
     constructor(props) {
         super(props)
@@ -89,7 +93,7 @@ class CandleStickChartWithDarkTheme extends React.Component {
 
     render() {
         const height = 200
-        const { type, data: initialData, width, ratio } = this.props
+        const { type, data: initialData, width, ratio, chartType } = this.props
 
         const margin = { left: 70, right: 70, top: 20, bottom: 30 }
 
@@ -112,6 +116,25 @@ class CandleStickChartWithDarkTheme extends React.Component {
         const start = xAccessor(last(data))
         const end = xAccessor(data[Math.max(0, data.length - 150)])
         const xExtents = [start, end]
+
+        let lineSeries = null
+        let volumeBarFill = '#808080'
+
+        if (chartType === ReactChartType.LINE) {
+            lineSeries = <LineSeries
+                yAccessor={d => d.close}
+                strokeDasharray="Solid" />
+            volumeBarFill = d => d.close > d.open ? "#00ffcc" : "#ff7970"
+        }
+
+        let candlestickSeries = null
+        if (chartType === ReactChartType.CANDLESTICK) {
+            candlestickSeries = <CandlestickSeries
+                stroke={d => d.close > d.open ? "#00ffcc" : "#ff7970"}
+                wickStroke={d => d.close > d.open ? "#00ffcc" : "#ff7970"}
+                fill={d => d.close > d.open ? "#00ffcc" : "#ff7970"} />
+        }
+
         return (
             <ChartCanvas height={260}
                 width={width}
@@ -138,13 +161,11 @@ class CandleStickChartWithDarkTheme extends React.Component {
                 >
                     <YAxis axisAt="right" orient="right" ticks={5} {...yGrid} inverted={true}
                         stroke="#C0C0C0" tickStroke="#C0C0C0" />
-                    <XAxis axisAt="bottom" orient="bottom" showTicks={true} ticks={6} outerTickSize={0}
+                    <XAxis axisAt="bottom" orient="bottom" showTicks={true} ticks={3} outerTickSize={0}
                         stroke="#C0C0C0" tickStroke="#C0C0C0" />
 
-                    <CandlestickSeries
-                        stroke={d => d.close > d.open ? "#00ffcc" : "#ff7970"}
-                        wickStroke={d => d.close > d.open ? "#00ffcc" : "#ff7970"}
-                        fill={d => d.close > d.open ? "#00ffcc" : "#ff7970"} />
+                    {candlestickSeries}
+                    {lineSeries}
 
                     <OHLCTooltip ohlcFormat={format(".8f")} volumeFormat={format(".3f")} textFill={'#C0C0C0'} forChart={1} origin={[-40, -10]} />
 
@@ -168,21 +189,22 @@ class CandleStickChartWithDarkTheme extends React.Component {
                         stroke='#C0C0C0' tickStroke="#C0C0C0" />
                     <BarSeries
                         yAccessor={d => d.volume}
-                        fill={'#808080'} />
+                        fill={volumeBarFill}
+                    />
                 </Chart>
             </ChartCanvas>
         )
     }
 }
-CandleStickChartWithDarkTheme.propTypes = {
+ChartRenderer.propTypes = {
     data: PropTypes.array.isRequired,
     width: PropTypes.number.isRequired,
     ratio: PropTypes.number.isRequired,
     type: PropTypes.oneOf(["svg", "hybrid"]).isRequired,
 }
 
-CandleStickChartWithDarkTheme.defaultProps = {
+ChartRenderer.defaultProps = {
     type: "hybrid",
 }
 
-export default fitWidth(CandleStickChartWithDarkTheme)
+export default fitWidth(ChartRenderer)
---
+++ src/components/OrderBook/ReactChartType.js
@@ -0,0 +1,4 @@
+export default {
+    CANDLESTICK: "CANDLESTICK",
+    LINE: "CANDLE"
+}
