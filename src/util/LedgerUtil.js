2d4d9019c7afeff5ac7e6c62880487e9db03a0e6
Will Robertson <wjsrobertson@gmail.com>
2018-08-06 12:13:44 +0100

    Remove our hacked up version of the LedgerWeb3 sub provider since they have fixed the address issue in more recent versions

--- src/util/LedgerUtil.js
+++ src/util/LedgerUtil.js
@@ -1,5 +1,6 @@
 import * as Web3 from 'web3'
-import createLedgerSubprovider from "../hacks/LedgerWeb3SubProvider"
+
+import createLedgerSubprovider from "@ledgerhq/web3-subprovider"
 import TransportU2F from "@ledgerhq/hw-transport-u2f"
 import ProviderEngine from "web3-provider-engine/dist/es5/index"
 import RpcSubprovider from "web3-provider-engine/dist/es5/subproviders/rpc"
@@ -7,17 +8,16 @@ import RpcSubprovider from "web3-provider-engine/dist/es5/subproviders/rpc"
 export function getLedgerWeb3(deriviationPath, web3Url, networkId) {
     const engine = new ProviderEngine()
     const getTransport = () => TransportU2F.create()
-    // We need our own version of createLedgerSubprovider since the Ledger provided one has a bug with
-    // address lookup when using web3 1.0.x - TODO - file a bug report - WR
+
     const ledger = createLedgerSubprovider(getTransport, {
         path: deriviationPath,
         networkId: networkId,
         accountsLength: 5
     })
+
     engine.addProvider(ledger)
     engine.addProvider(new RpcSubprovider({rpcUrl: web3Url}))
     engine.start()
-    //engine.stop()
 
     // engineWithNoEventEmitting is needed because infura doesn't support newBlockHeaders event :( - WR
     // https://github.com/ethereum/web3.js/issues/951
