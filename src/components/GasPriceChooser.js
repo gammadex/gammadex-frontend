c6c75da60614aabaf41ab7cb20e5d1a64c52a02a
Alex Mellusco <alexmellusco@gmail.com>
2018-08-08 13:57:30 +0100

    Add comment to Estimate Gas button, fixes #180

--- src/components/GasPriceChooser.js
+++ src/components/GasPriceChooser.js
@@ -3,15 +3,15 @@ import Slider from 'rc-slider'
 import 'rc-slider/assets/index.css'
 import GasPriceStore from '../stores/GasPriceStore'
 import * as GasActions from "../actions/GasActions"
-import {Box, BoxSection} from "./CustomComponents/Box"
-import {Popover, PopoverBody} from 'reactstrap'
-import {gweiToWei, weiToGwei, gweiToEth, baseWeiToEth, safeBigNumber} from "../EtherConversion"
+import { Box, BoxSection } from "./CustomComponents/Box"
+import { Popover, PopoverBody } from 'reactstrap'
+import { gweiToWei, weiToGwei, gweiToEth, baseWeiToEth, safeBigNumber } from "../EtherConversion"
 import * as _ from "lodash"
-import {OperationCosts} from "../ContractOperations"
+import { OperationCosts } from "../ContractOperations"
 import WalletStore from "../stores/WalletStore"
 import AccountStore from "../stores/AccountStore"
 import EtherDeltaWeb3 from "../EtherDeltaWeb3"
-import {setFavourite} from "../util/FavouritesDao"
+import { setFavourite } from "../util/FavouritesDao"
 import Favourites from "../util/Favourites"
 import Round from "./CustomComponents/Round"
 import BigNumber from 'bignumber.js'
@@ -57,7 +57,7 @@ export default class GasPriceChooser extends React.Component {
     }
 
     saveGasPrices() {
-        const {safeLowWei, averageWei, fastWei, fastestWei} = GasPriceStore.getPrices()
+        const { safeLowWei, averageWei, fastWei, fastestWei } = GasPriceStore.getPrices()
 
         this.setState({
             safeLowWei,
@@ -102,7 +102,7 @@ export default class GasPriceChooser extends React.Component {
     }
 
     getTimeDescription() {
-        const {safeLowWei, averageWei, fastWei, currentGasPriceWei} = this.state
+        const { safeLowWei, averageWei, fastWei, currentGasPriceWei } = this.state
 
         if (!safeLowWei || !averageWei || !fastWei || !currentGasPriceWei) {
             return null
@@ -120,7 +120,7 @@ export default class GasPriceChooser extends React.Component {
     }
 
     onEstimateGasChange = (event) => {
-        const {estimateGas} = this.state
+        const { estimateGas } = this.state
         EtherDeltaWeb3.estimateGas = !estimateGas
         setFavourite(Favourites.ESTIMATE_GAS, !estimateGas)
         this.setState({
@@ -129,7 +129,7 @@ export default class GasPriceChooser extends React.Component {
     }
 
     render() {
-        const {popoverOpen, currentGasPriceWei, ethereumPriceUsd, fastWei, averageWei, estimateGas, accountState} = this.state
+        const { popoverOpen, currentGasPriceWei, ethereumPriceUsd, fastWei, averageWei, estimateGas, accountState } = this.state
 
 
         const averageGwei = GasPriceChooser.safeWeiToGwei(averageWei)
@@ -144,7 +144,7 @@ export default class GasPriceChooser extends React.Component {
         let badgeType = "badge-success"
         let walletBalanceEth = "0"
         if (accountState) {
-            const {walletBalanceEthWei} = accountState
+            const { walletBalanceEthWei } = accountState
             walletBalanceEth = baseWeiToEth(walletBalanceEthWei).toString()
             availableTrades = `${Number(safeBigNumber(walletBalanceEth).div(safeBigNumber(gasCostsEth.TAKE_ORDER)).dp(0, BigNumber.ROUND_FLOOR))}`
             availableEth = <Round fallback="-">{walletBalanceEth}</Round>
@@ -157,10 +157,10 @@ export default class GasPriceChooser extends React.Component {
         return (
             <li className="nav-item dropdown">
 
-                <button className="nav-link dropdown-toggle btn btn-link" style={{"height": "36px"}} id="gasPrice" aria-haspopup="true" aria-expanded="false" onClick={this.toggleGasPrice}>
-                    <span style={{"fontSize": "140%", "verticalAlign": "middle"}} className="fas fa-tachometer-alt mr-2">&nbsp;</span>
+                <button className="nav-link dropdown-toggle btn btn-link" style={{ "height": "36px" }} id="gasPrice" aria-haspopup="true" aria-expanded="false" onClick={this.toggleGasPrice}>
+                    <span style={{ "fontSize": "140%", "verticalAlign": "middle" }} className="fas fa-tachometer-alt mr-2">&nbsp;</span>
                     Gas Price:
-                    <div style={{"width": "20px", "display": "inline-block", "text-align": "right"}}>{currentGasPriceGwei}</div>
+                    <div style={{ "width": "20px", "display": "inline-block", "text-align": "right" }}>{currentGasPriceGwei}</div>
                     &nbsp;Gwei&nbsp;
                 </button>
 
@@ -170,7 +170,7 @@ export default class GasPriceChooser extends React.Component {
                             <Box title="Gas Price Per Unit">
                                 <BoxSection>
 
-                                    <span className={"badge " + badgeType} style={{"padding": "7px", "width": "100%", "fontSize": "100%", "verticalAlign": "middle"}}>Balance: {availableEth} ETH <br/>Enough gas for {walletBalanceEth !== "0" ? 'approx.' : ''} {availableTrades} trades</span>
+                                    <span className={"badge " + badgeType} style={{ "padding": "7px", "width": "100%", "fontSize": "100%", "verticalAlign": "middle" }}>Balance: {availableEth} ETH <br />Enough gas for {walletBalanceEth !== "0" ? 'approx.' : ''} {availableTrades} trades</span>
 
 
                                     &nbsp;&nbsp;
@@ -185,12 +185,12 @@ export default class GasPriceChooser extends React.Component {
                                         </div>
                                         <div className="col-lg-4">
                                             <div className="float-right"><a href="#" onClick={this.onUseExpensive}>Expensive</a></div>
-                                            <br/>
+                                            <br />
                                             <div className="float-right">Faster</div>
                                         </div>
                                     </div>
 
-                                    <Slider min={1} max={200} defaultValue={averageGwei} onChange={this.onSliderChange} value={currentGasPriceGwei}/>
+                                    <Slider min={1} max={200} defaultValue={averageGwei} onChange={this.onSliderChange} value={currentGasPriceGwei} />
 
                                     <div className="row">
                                         <div className="col-lg-12 text-center">
@@ -201,7 +201,7 @@ export default class GasPriceChooser extends React.Component {
                                     <div className="row">
                                         <div className="col-lg-12 text-center">
                                             {timeDescription} transaction speed estimate from <a target="_blank" rel="noopener noreferrer"
-                                                                                                 href="https://ethgasstation.info/">ethgasstation.info</a>
+                                                href="https://ethgasstation.info/">ethgasstation.info</a>
                                         </div>
                                     </div>
                                 </BoxSection>
@@ -209,52 +209,57 @@ export default class GasPriceChooser extends React.Component {
                                     <div>
                                         <table className="table">
                                             <tbody>
-                                            <tr>
-                                                <th>Operation</th>
-                                                <th>Gas Fee (ETH)</th>
-                                                <th>Gas Fee (USD)</th>
-                                            </tr>
-                                            <tr>
-                                                <td>Trade</td>
-                                                <td>{gasCostsEth.TAKE_ORDER}</td>
-                                                <td>${gasCostsUsd.TAKE_ORDER}</td>
-                                            </tr>
-                                            <tr>
-                                                <td>Cancel Order</td>
-                                                <td>{gasCostsEth.CANCEL_ORDER}</td>
-                                                <td>${gasCostsUsd.CANCEL_ORDER}</td>
-                                            </tr>
-                                            <tr>
-                                                <td>Deposit ETH</td>
-                                                <td>{gasCostsEth.DEPOSIT_ETH}</td>
-                                                <td>${gasCostsUsd.DEPOSIT_ETH}</td>
-                                            </tr>
-                                            <tr>
-                                                <td>Deposit Token</td>
-                                                <td>{gasCostsEth.DEPOSIT_TOK}</td>
-                                                <td>${gasCostsUsd.DEPOSIT_TOK}</td>
-                                            </tr>
-                                            <tr>
-                                                <td>Withdraw ETH</td>
-                                                <td>{gasCostsEth.WITHDRAW_ETH}</td>
-                                                <td>${gasCostsUsd.WITHDRAW_ETH}</td>
-                                            </tr>
-                                            <tr>
-                                                <td>Withdraw Token</td>
-                                                <td>{gasCostsEth.WITHDRAW_TOK}</td>
-                                                <td>${gasCostsUsd.WITHDRAW_TOK}</td>
-                                            </tr>
+                                                <tr>
+                                                    <th>Operation</th>
+                                                    <th>Gas Fee (ETH)</th>
+                                                    <th>Gas Fee (USD)</th>
+                                                </tr>
+                                                <tr>
+                                                    <td>Trade</td>
+                                                    <td>{gasCostsEth.TAKE_ORDER}</td>
+                                                    <td>${gasCostsUsd.TAKE_ORDER}</td>
+                                                </tr>
+                                                <tr>
+                                                    <td>Cancel Order</td>
+                                                    <td>{gasCostsEth.CANCEL_ORDER}</td>
+                                                    <td>${gasCostsUsd.CANCEL_ORDER}</td>
+                                                </tr>
+                                                <tr>
+                                                    <td>Deposit ETH</td>
+                                                    <td>{gasCostsEth.DEPOSIT_ETH}</td>
+                                                    <td>${gasCostsUsd.DEPOSIT_ETH}</td>
+                                                </tr>
+                                                <tr>
+                                                    <td>Deposit Token</td>
+                                                    <td>{gasCostsEth.DEPOSIT_TOK}</td>
+                                                    <td>${gasCostsUsd.DEPOSIT_TOK}</td>
+                                                </tr>
+                                                <tr>
+                                                    <td>Withdraw ETH</td>
+                                                    <td>{gasCostsEth.WITHDRAW_ETH}</td>
+                                                    <td>${gasCostsUsd.WITHDRAW_ETH}</td>
+                                                </tr>
+                                                <tr>
+                                                    <td>Withdraw Token</td>
+                                                    <td>{gasCostsEth.WITHDRAW_TOK}</td>
+                                                    <td>${gasCostsUsd.WITHDRAW_TOK}</td>
+                                                </tr>
                                             </tbody>
                                         </table>
 
                                         <strong>The above costs are a rough guideline only</strong>. Actual gas cost can vary depending on a number of factors, including the token being operated on.
-                                        <hr/>
+                                        <hr />
 
                                         <form className="form-inline">
                                             <div className="form-check form-check-inline">
-                                                <input className="form-check-input" type="checkbox" id="estimateGasCheckbox" onChange={this.onEstimateGasChange} value={"true"} checked={estimateGas}/>
+                                                <input className="form-check-input" type="checkbox" id="estimateGasCheckbox" onChange={this.onEstimateGasChange} value={"true"} checked={estimateGas} />
                                                 <label className="form-check-label" htmlFor="estimateGasCheckbox">&nbsp;Estimate gas limit</label>
+
                                             </div>
+                                            <br />
+                                            <small class="form-text text-muted">
+                                                Uncheck to use a default gas limit of 250000 for all transactions, which is excessive for most actions.
+                                            </small>
                                         </form>
                                     </div>
                                 </BoxSection>
