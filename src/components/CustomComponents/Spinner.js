75058b676052d22d9c2ff19829bdafb13c904c40
Will Robertson <wjsrobertson@gmail.com>
2018-08-14 15:25:51 +0100

    Move spinner on mobile to bottom right of screen

--- src/components/CustomComponents/Spinner.js
+++ src/components/CustomComponents/Spinner.js
@@ -2,6 +2,6 @@ import React from "react"
 
 export default class Spinner extends React.Component {
     render() {
-        return <span className="ml-1"><i className="fas fa-lg fa-cog fa-spin"></i></span>
+        return <span className="ml-1 market-response-spinner"><i className="fas fa-lg fa-cog fa-spin"></i></span>
     }
 }
