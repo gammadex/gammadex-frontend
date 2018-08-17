240db69fc5c8d325e7ab168359b7b9f1fb8c1c5e
Alex Mellusco <alexmellusco@gmail.com>
2018-08-17 16:06:29 +0100

    Rough cut of Terms of Use, #179

---
+++ src/legal/TermsOfUseWording.js
@@ -0,0 +1,95 @@
+const wording = `
+Published: August 16, 2018
+
+By accessing or using the GammaDEX website, you (the client, User, Yourself, Users) agree to be bound by these terms of use ("Terms of Use", "Terms") outlined by GammaDEX.com (GammaDEX, We, Us, Our, Exchange, Trading Platform Service, Website, Site). **These Terms of Use affect your legal rights and obligations. If you do not agree to be bound by all of these Terms of Use, do not access or use the service**
+
+##### Amendments to the GammaDEX Terms of Use
+
+GammaDEX reserves the right from time to time to modify the Terms of Use. Any changes or modifications will be effective immediately upon posting to the website. It is your responsibility to review the amended Terms. If you continue to use the Service, you will be bound by the updated Terms of Use. If you do not want to agree to these or any updated Terms of Use, you must stop using the service immediately. Your continued use of this Site acts as acceptance of the last revised Agreement.
+
+##### Description of Service
+
+GammaDEX provides an online platform for users to trade Tokens ("Digital Assets", "Funds", "Ethereum-based Tokens") based on the Ethereum Blockchain. The price of a trade is determined by the users involved in the trade, the buyer and seller. GammaDEX is not the buyer or seller of these trades.
+
+The GammaDEX trading platform provides the user with convenient access to: infura.io (a third party Public Ethereum Access Provider), the EtherDelta.com Exchange Smart Contract ("EtherDelta", "EtherDelta Smart Contract", "Third Party Smart Contract", "Smart Contract"), Third Party EtherDelta-compatible Order lists ("Order Books"), the Ethereum Network, Ethereum-based Tokens, all collectively third party services ("Third Party Applications") unaffiliated with GammaDEX. User funds are held directly in the Third Party Smart Contract. Only the user who owns those funds can deposit, withdraw or exchange those funds with other users from the Third Party Smart Contract. GammaDEX does not have access to user funds, nor can GammaDEX execute trades or create orders on the user's behalf. The Third Party Smart Contract runs on the Ethereum Main Network.
+
+###### Depositing and Withdrawing Tokens
+
+Through the GammaDEX website user's can send ("Deposit") Ether and Ethereum-based Tokens from a supported digital wallet (private key-based, JSON keystore file, MetaMask or Hardware Wallet) to the EtherDelta Smart Contract. Deposits and Withdrawals take the form of Ethereum Transactions authorized by You the wallet owner and submitted to the Ethereum Network. Once funds are deposited to the EtherDelta Smart Contract, they can be exchanged with other users either by (a) making Orders and waiting for another user to take your Order, or (b) taking another user's existing Order.
+
+GammaDEX does not have access to your funds and is not able to deposit to or withdraw from the Smart Contract without your authorization. By using GammaDEX to deposit or withdraw Ether or Ethereum-based Tokens you do so at your own risk. GammaDEX does not guarantee the successful execution of, accuracy, timeliness, validity or integrity of Ethereum Transactions sent on Your behalf. You agree not to hold GammaDEX responsible for losses of any form incurred in any way from depositing or withdrawing Ether or digital assets on the website.
+
+###### Making Orders
+
+There are two parties ("users") involved when trading Tokens on the Exchange Smart Contract: An Order Maker and Order Taker.
+
+GammaDEX allows the user to create EtherDelta-compatible Orders. An Order is a message describing an intent to trade a defined amount of one Token for a defined amount of another Token. The message is digitally signed by the User to prove authenticity. The order has a user defined expiry time (measured in Ethereum blocks) and a price implied by the Token amounts defined by the user.
+
+Once an Order is created, no trade or tranfer of tokens happens immediately. The Maker of the Order must wait for a taker to create a trade against an order at which point Tokens will move between the maker and taker.
+
+GammaDEX displays lists of buy and sell orders ("Order Books", "Books", "Book") for Tokens. The orders that appear in the Book are gathered from GammaDEX directly and other third parties that gather EtherDelta-compatible orders. Orders created on the GammaDEX website are also published to other third parties, though this happens with no guarantee. By using this service you agree to Orders being published to third parties in this way and with no guarantee.
+
+You understand and agree that GammaDEX does not guarantee the successful execution of, accuracy, timeliness, validity or integrity of the Orders created through or displayed on the website. You create and digitally sign orders at your own risk. You agree not to hold GammaDEX responsible for losses of any form incurred in any way from making orders on the website.
+
+###### Cancelling Orders
+
+A user can cancel an order they have created by authorizing and sending an Ethereum Transaction to the Ethereum Network. By using GammaDEX to cancel orders, you do so at your own risk. GammaDEX does not guarantee the successful execution of, accuracy, timeliness, validity or integrity of Ethereum Transactions sent on Your behalf. You agree not to hold GammaDEX responsible for losses of any form incurred in any way from cancelling orders on the website.
+
+###### Taking Orders
+
+GammaDEX allows the user to trade against Orders displayed in the Order Book. A trade is sent through the GammaDEX website as an Ethereum Transactions authorized by You the wallet owner and submitted to the Ethereum Network. By using GammaDEX to trade tokens with other users, you do so at your own risk. GammaDEX does not guarantee the successful execution of, accuracy, timeliness, validity or integrity of Ethereum Transactions sent on Your behalf. You agree not to hold GammaDEX responsible for losses of any form incurred in any way from Trading digital assets on the website.
+
+###### Market Data
+
+Users of GammaDEX are able to view past market trades for a Token traded using the Smart Contract. This market data is extracted from the Smart Contract and Ethereum blockchain. GammaDEX does not guarantee the accuracy, timeliness, validity or integrity of the market data it displays on the website. You agree not to hold GammaDEX responsible for losses of any form incurred in any way from viewing market data or making trading decisions on the website.
+
+###### User History
+
+Users of GammaDEX are able to view their trade, deposit and withdrawal history for a Ether and Ethereum-based tokens successfully submitted to the Smart Contract. GammaDEX does not guarantee the accuracy, timeliness, validity or integrity of the user history it displays on the website. You agree not to hold GammaDEX responsible for losses of any form incurred in any way from viewing user trade, deposit or withdrawal history on the website.
+
+##### Third Party Services
+
+You acknowledge and agree that: (a) You must use Your own discretion when You access Third Party Applications and Services through GammaDEX. (b) You should read the terms and conditions and privacy policies associated with any Third Party Applications as they will govern Your use of the Third Party Applications; and (c) GammaDEX does not own or control any of these Third Party Applications, and You will not hold GammaDEX responsible or liable for any such Third Party Applications or acts or omissions of the Developers of the Third Party Applications, under any circumstances. GammaDEX does not in any way warrant the accuracy, reliability, security, completeness, usefulness, non-infringement, or quality of any Third Party Applications.
+
+You agree that You bear all risks associated with using or relying on Third Party Applications. If You have any questions about Third Party Applications or the terms that govern the use of the Third Party Applications, You should contact Third Party Developer of the Third Party Application directly. GammaDEX is not responsible or liable for any loss or damage of any sort incurred as the result of Third Party Applications.
+
+##### Tokens
+
+GammaDEX allows users to trade Ethereum-based tokens that conform to the ERC20 Token Standard Interface. This can include other Ethereum Token Standards that build on top of ERC20. GammaDEX does not guarantee the accuracy or authenticity of the Tokens you can view or trade on the platform and you should thoroughly research the Token contract details you intend to trade. GammaDEX does not own or control any of these Tokens, and You agree to not hold GammaDEX responsible or liable for losses incurred from trading any such Tokens or acts or omissions of the Token Developers, under any circumstances. GammaDEX does not in any way warrant the accuracy, reliability, security, completeness, usefulness, non-infringement, or quality of the Tokens listed on the platform.
+
+##### Fees
+
+GammaDEX does not charge a fee for using it's services.
+
+Third Party Applications may charge the user a fee for actions including (but not limited to) executing a trade. You acknowledge and agree that all costs and fees associated with trading through Third Party Applictions shall be at your own cost and responsibility. You should read the fee schedule associated with any Third Party Applications.
+
+The Ethereum Network will impose a charge for executing transactions on your behalf, known as 'gas'. Gas is payable directly from the User's wallet to the Ethereum Network, when a ransaction is authorized and sent from GammaDEX. You acknowledge and agree that all gas fees shall be at your own cost and responsibility. GammaDEX does not guarantee the accuracy, timeliness, validity or integrity of gas prices and gas limits set and used when submitting transactions and you agree not to hold GammaDEX responsible for losses of any form incurred from these gas prices or gas limits.
+
+##### Support and Maintenance
+
+You understand, agree, and accept that GammaDEX has no obligation to maintain, support, upgrade, or update the Service, or to provide all or any specific content or functionality through the Service.
+
+##### Taxes
+
+It is Your sole responsibility to determine what, if any, taxes apply to the payments You make or receive, and to collect, report, and remit the correct tax to the appropriate tax authority. GammaDEX is not responsible for determining whether taxes apply to your transaction, or for collecting, reporting, or remitting any taxes arising from any transaction.
+
+##### Accuracy and Timeliness of Content
+
+GammaDEX does not guarantee the accuracy, completeness, reliability of any content. You acknowledge and agree to bear the risk of using the content of this website.
+
+##### Limitation of Liability and Disclaimer
+
+You acknowledge and consent that you agree not to hold GammaDEX accountable or liable if for any other reason the GammaDEX website is unavailable for any period of time or if the connection between your device / computer and GammaDEX is terminated, delayed or interupted. GammaDEX does not guarantee continuous, uninterrupted or secure access to any part of the Services, and operation of our site may be interfered with by numerous factors outside of our control.
+
+You acknowledge and agree not to hold GammaDEX responsible for any problems or loss that may result from the use of Third Party Applications and Services, including (but not limited to): the EtherDelta Smart Contract, infura.io and EtherDelta-compatible Order Books.
+
+You hereby expressly understand and agree that GammaDEX will not be liable for any indirect, special, incidental, punitive, exemplary or consequential damages for loss of profits, use, data, or business associated with the use of the Service.
+
+You agree to defend, indemnify and hold harmless GammaDEX from and against any claims, damages, costs, liabilities and expenses arising out of your use of the Services in any form.
+
+The Services of GammaDEX are provided as is and without any representation of warranty, whether express, implied, or statutory. GammaDEX specifically disclaim any implied warranties of title, merchantability, fitness for a particular purpose and non-infringement.
+
+You are responsible for your own account, funds, digital asset wallets, keystore files and private keys. You are responsible for your own trading decisions, and the details and mechanics of the tokens you trade. GammaDEX is not responsible for your decisions, actions, injury, loss of business, monetary loss or loss of Digital Assets or Tokens that result from using GammaDEX. GammaDEX makes no guarantee about the tokens that you trade using GammaDEX. GammaDEX does not hold your funds, have access to your funds without your authorization and will not offer reimbursement for any losses incurred either directly or indirectly using the Service. While the information contained on GammaDEX is periodically updated, no guarantee is given that the information provided on GammaDEX is correct, complete, or up-to-date. By using GammaDEX, you acknowledge this and agree to these terms.
+
+`
+export default wording
