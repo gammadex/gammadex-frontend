2cea8a5b40359e39c23e6110b86a64a59972cdeb
Alex Mellusco <alexmellusco@gmail.com>
2018-08-19 10:48:47 +0100

    Disclaimer, Privacy and T&C Page, fixes #179, fixes #79

--- src/components/GreetingDisclaimer/DisclaimerModal.js
+++ src/components/GreetingDisclaimer/DisclaimerModal.js
@@ -1,19 +1,15 @@
 import React from "react"
 import { Modal, ModalHeader, ModalBody, ModalFooter, Alert } from 'reactstrap'
-import ReactMarkdown from 'react-markdown'
-import wording from './DisclaimerWording'
 
 export default class DisclaimerModal extends React.Component {
 
     constructor(props) {
         super(props)
-        const lines = wording.split('\n')
-        const fileVersion = lines.splice(0, 1)[0]
-        const accepted = localStorage.acceptedDisclaimerVersion != null && localStorage.acceptedDisclaimerVersion !== "" && Number(localStorage.acceptedDisclaimerVersion) >= Number(fileVersion) ? true : false
+        const disclaimerVersion = 0.2
+        const accepted = localStorage.acceptedDisclaimerVersion != null && localStorage.acceptedDisclaimerVersion !== "" && Number(localStorage.acceptedDisclaimerVersion) >= Number(disclaimerVersion) ? true : false
         this.state = {
             showModal: !accepted,
-            disclaimerText: wording,
-            disclaimerVersion: fileVersion
+            disclaimerVersion
         }
     }
 
@@ -27,25 +23,21 @@ export default class DisclaimerModal extends React.Component {
     }
 
     render() {
-        const { showModal, disclaimerText } = this.state
+        const { showModal } = this.state
 
-        const lines = disclaimerText.split('\n')
-        const meta = lines.splice(0, 2)
-        const disclaimerTextNoMeta = lines.join('\n')
-        return <Modal size="lg" isOpen={showModal} toggle={this.hideModal} keyboard>
-            <ModalHeader>
-                <div className="disclaimer-header">GammaDEX Disclaimer</div>
-                <div className="disclaimer-version">Version: {meta[0]} (Published: {meta[1]})</div>
-            </ModalHeader>
+        return <Modal size="md" isOpen={showModal} toggle={this.hideModal} centered={true} keyboard>
             <form onSubmit={this.handleAgree}>
                 <ModalBody>
-                    <div className='disclaimer-md'>
-                        <ReactMarkdown source={disclaimerTextNoMeta} />
+                    <div className='disclaimer'>
+                        <img id="disclaimer-logo" src="/gammadex_logo.svg"/>
+                        <div id="disclaimer-content">
+                            <span>GammaDEX is an advanced decentralized exchange for trading Ethereum tokens. 
+                                By choosing "I AGREE" below, you agree to GammaDEX's <a target="_blank" rel="noopener noreferrer" href={`/terms.html`}
+                      >{"Terms of Use and Privacy Policy"}</a>.</span>
+                        </div>
+                        <input className="btn btn-primary btn-lg" type="submit" value="I AGREE" />
                     </div>
                 </ModalBody>
-                <ModalFooter>
-                    <input className="btn btn-primary" type="submit" value="I agree" />
-                </ModalFooter>
             </form>
         </Modal>
     }
--- src/components/SupportDropdown.js
+++ src/components/SupportDropdown.js
@@ -46,6 +46,8 @@ export default class SupportDropdown extends React.Component {
                         <a className="dropdown-item" target="_blank" rel="noopener noreferrer" href="https://discord.gg/RANPVZ5"><i className="fab fa-discord"></i><span className="support-text">Discord chat</span></a>
                         <a className="dropdown-item" target="_blank" rel="noopener noreferrer" href="https://twitter.com/_GammaDEX"><i className="fab fa-twitter"></i><span className="support-text">Twitter</span></a>
                         <a className="dropdown-item" target="_blank" rel="noopener noreferrer" href="https://www.reddit.com/r/GammaDEX"><i className="fab fa-reddit-alien"></i><span className="support-text">Reddit</span></a>
+                        <div className="dropdown-divider"></div>
+                        <a className="dropdown-item" target="_blank" rel="noopener noreferrer" href="/terms.html"><i className="fas fa-pen-nib"></i><span className="support-text">Terms of Use</span></a>
                     </PopoverBody>
                 </Popover>
             </li>
