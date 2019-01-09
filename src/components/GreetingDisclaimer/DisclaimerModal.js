619ecdc864a9203a3ca2f3dae378d2ed1be09135
Will Robertson <wjsrobertson@gmail.com>
2019-01-09 15:57:33 +0000

    Add demo mode to agreement modal

--- src/components/GreetingDisclaimer/DisclaimerModal.js
+++ src/components/GreetingDisclaimer/DisclaimerModal.js
@@ -1,6 +1,8 @@
 import React from "react"
-import { Modal, ModalHeader, ModalBody, ModalFooter, Alert } from 'reactstrap'
+import {Modal, ModalHeader, ModalBody, ModalFooter, Alert} from 'reactstrap'
 import {trackEvent} from '../../util/Analytics'
+import Config from '../../Config'
+import Conditional from "../CustomComponents/Conditional"
 
 export default class DisclaimerModal extends React.Component {
 
@@ -10,14 +12,15 @@ export default class DisclaimerModal extends React.Component {
         const accepted = localStorage.acceptedDisclaimerVersion != null && localStorage.acceptedDisclaimerVersion !== "" && Number(localStorage.acceptedDisclaimerVersion) >= Number(disclaimerVersion) ? true : false
         this.state = {
             showModal: !accepted,
-            disclaimerVersion
+            disclaimerVersion,
+            isDemoMode: Config.isDemoMode(),
         }
     }
 
     handleAgree = (event) => {
         event.preventDefault()
 
-        trackEvent("user","accepted disclaimer")
+        trackEvent("user", "accepted disclaimer")
 
         localStorage.acceptedDisclaimerVersion = this.state.disclaimerVersion
         this.setState({
@@ -26,7 +29,7 @@ export default class DisclaimerModal extends React.Component {
     }
 
     render() {
-        const { showModal } = this.state
+        const {showModal, isDemoMode} = this.state
 
         return <Modal size="md" isOpen={showModal} toggle={this.hideModal} centered={true} keyboard>
             <form onSubmit={this.handleAgree}>
@@ -34,11 +37,19 @@ export default class DisclaimerModal extends React.Component {
                     <div className='disclaimer'>
                         <img id="disclaimer-logo" src="/gammadex_logo.svg"/>
                         <div id="disclaimer-content">
-                            <span>GammaDEX is an advanced decentralized exchange for trading Ethereum tokens. 
-                                By choosing "I AGREE" below, you agree to GammaDEX's <a target="_blank" rel="noopener noreferrer" href={`/terms.html`}
-                      >{"Terms of Use and Privacy Policy"}</a>.</span>
+
+                            <div>GammaDEX is an advanced decentralized exchange for trading Ethereum tokens</div>
+
+                            <Conditional displayCondition={isDemoMode}>
+                                <div className="m-3"><strong>Running in demo mode - TRADING IS DISABLED</strong></div>
+                            </Conditional>
+
+                            <Conditional displayCondition={!isDemoMode}>
+                                <div>By choosing "I AGREE" below, you agree to GammaDEX's <a target="_blank" rel="noopener noreferrer" href={`/terms.html`}>{"Terms of Use and Privacy Policy"}</a>.</div>
+                            </Conditional>
                         </div>
-                        <input className="btn btn-primary btn-lg" type="submit" value="I AGREE" />
+
+                        <input className="btn btn-primary btn-lg" type="submit" value="I AGREE"/>
                     </div>
                 </ModalBody>
             </form>
